d3dfb8b9005ac3ad6abc717c78b037ea7fa6c6c6
swnirk <54669541+swnirk@users.noreply.github.com>
2020-04-02 13:03:55 +0300

    Update write_read.c

--- write_read.c
+++ write_read.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
 #include "pdp11.h"
+#include <stdarg.h>
+
 #define MEMSIZE (64*1024)
+
 
 byte mem [MEMSIZE];
 
@@ -20,7 +23,7 @@ word w_read (Adress a) {
 	word w = ((word)mem[a+1]) << 8;
 	//printf ("w = %x\n", w);
 	w = w | mem[a];
-	printf ("//%04hx//", w);
+	trace ("//%04hx//\n", w);
 	return w;
 }
 
@@ -29,3 +32,9 @@ void w_write (Adress adr, word w) {
 	mem[adr] = (byte)(w);
 	mem[adr+1] = (byte)(w >> 8);
 }
+
+word bw_read (Adress a, int nb, int r) {
+	
+	return (((nb)|(r == 6)|(r == 7)) ? w_read(a) : b_read(a));
+}
+
